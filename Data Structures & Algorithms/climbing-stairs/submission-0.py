89e550ea2207998f4644468950feacd1bd73070e
Nithyashree K J <77705699+Nithya15aa@users.noreply.github.com>
2026-09-02 21:33:59 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 1: return 1
+        if n == 2: return 2 
+        f = [None]*(n+1)
+        f[1] = 1
+        f[2] = 2
+        f[0] = 0
+        for i in range(3, n+1):
+            f[i] =  f[i-1] + f[i-2]
+        
+        return f[n]
+        
+        
